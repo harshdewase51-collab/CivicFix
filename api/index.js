b9645ae1b1d9248028dd072c05d9be56de18c657
harshdewase51-collab <harshdewase51@gmail.com>
2026-09-20 22:28:32 +0530

    Redeploy with Production Neon DATABASE_URL

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-// CivicFix Serverless API Entrypoint - Updated 2026-09-20
+// CivicFix Serverless API Entrypoint - Redeploy with Production Neon DATABASE_URL
 const app = require('../server/src/server');
 
 module.exports = app;
